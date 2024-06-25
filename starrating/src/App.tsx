80f54a0cdadb2b5ca460917ba82ba31346f76f6c
= <=>
2024-06-25 23:59:03 +0530

    Added StarRating

---
+++ starrating/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import { FaStar } from "react-icons/fa";
+import "./App.css"
+function App() {
+  const[rating,setRating] = useState<number>(0);
+  const [hover,setHover] = useState<number>(0);
+  const handleClick =(index:number)=>{
+   setRating(index+1);
+  }
+  console.log(rating)
+  return (
+    <div className="app">
+     Star Rating
+     <div>
+    {[...Array(5)].map((_,index)=>{
+      return <span onMouseLeave={()=>setHover(rating)} onMouseEnter={()=>setHover(index+1)} style={{color:`${hover>index?"yellow":""}`}} onClick={()=>handleClick(index)} key={index}>
+        <FaStar />
+      </span>
+    })}
+    </div>
+    </div>
+  )
+}
+
+export default App
